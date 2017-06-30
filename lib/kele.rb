63ca2db67e013ab467c2b528f4a610a957df26d4
SWLivingstone <scottwlivingstone@gmail.com>
2017-06-30 16:07:39 -0700

    Checkpoint 2
    Add class Kele to retrive and store authentication token from bloc api.

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,3 +1,4 @@
+gem 'httparty', '~> 0.13'
 require 'httparty'
 
 class Kele
@@ -9,10 +10,17 @@ class Kele
   def initialize(email, password)
     @email = email
     @password = password
+    @parsed_response = JSON.parse(authentication_token.body, symbolize_names: true)
+    unless @parsed_response[:message]
+      @auth_token = @parsed_response[:auth_token]
+      print "Authentication successful."
+    else
+      print "Your password or email is invalid."
+    end
   end
 
   def authentication_token
-    options = {"email": @email, "password": @password}
-    self.class.post('/sessions', options)
+    options = {"email" => @email, "password" => @password}
+    self.class.post('/sessions', :body => options)
   end
 end
